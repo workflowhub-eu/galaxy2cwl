db5c81d4a1349d883c082dea94490b6ca4151573
bedroesb <bedro@psb.vib-ugent.be>
2020-04-09 21:29:55 +0200

    parameter_type

--- galaxy2cwl/get_cwl_interface.py
+++ galaxy2cwl/get_cwl_interface.py
@@ -61,9 +61,14 @@ def process_format1_json(wf_dict):
             wf_inputs[input_name] = input_details
         elif step_details['type'] == 'parameter_input':
             input_details = {}
-            input_details['format'] = 'parameter'
-            input_details['type'] = 'Parameter'
+            input_details['format'] = 'boolean'
+            state = json.loads(step_details['tool_state'])
+            input_details['type'] = state['parameter_type']
             input_name = step_index + '_' + 'Input Parameter'
+            if len(step_details['workflow_outputs']) > 0:
+                output_name = sanitize_source(step_details['workflow_outputs'][0]['output_name'])
+            else:
+                output_name = 'output'
             map_output_to_in_name[step_index + '_' + output_name] = input_name
             wf_inputs[input_name] = input_details
 
